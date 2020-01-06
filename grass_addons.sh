363a8ee44cdd68951c06eb47fabe92e91e45f450
Martin Landa <landa.martin@gmail.com>
2020-01-06 10:57:33 +0100

    fix grass addons compilation (py3)

--- grass_addons.sh
+++ grass_addons.sh
@@ -10,9 +10,10 @@ if test -z "$1"; then
     exit 1
 fi
 PLATFORM=$1
-export PATH=/c/osgeo4w${PLATFORM}/bin:/c/msys${PLATFORM}/usr/bin:/c/msys${PLATFORM}/mingw${PLATFORM}/bin:${PATH}
-export PYTHONPATH=
+# export PATH=/c/osgeo4w${PLATFORM}/bin:/c/msys${PLATFORM}/usr/bin:/c/msys${PLATFORM}/mingw${PLATFORM}/bin:${PATH}
+# export PYTHONPATH=
 export LANGUAGE=C
+export OSGEO4W_ROOT_MSYS="/c/OSGeo4W${PLATFORM}"
 
 SRC_PATH=/c/msys${PLATFORM}/usr/src/grass-addons
 GISBASE_PATH=/c/msys${PLATFORM}/usr/src
@@ -26,20 +27,41 @@ fi
 cd $SRC_PATH
 git pull
 
+fetchenv() {
+    local IFS
+    IFS=
+    batch=$1
+    shift
+    srcenv=$(mktemp /tmp/srcenv.XXXXXXXXXX)
+    dstenv=$(mktemp /tmp/dstenv.XXXXXXXXXX)
+    diffenv=$(mktemp /tmp/diffenv.XXXXXXXXXX)
+    args="$@"
+    cmd.exe //c set >$srcenv
+    cmd.exe //c "call `cygpath -w $batch` $args \>nul 2\>nul \& set" >$dstenv
+    diff -u $srcenv $dstenv | sed -f /usr/src/grass79/mswindows/osgeo4w/envdiff.sed >$diffenv
+    . $diffenv
+    PATH=$PATH:/usr/bin:/mingw${MINGW_POSTFIX}/bin/:$PWD/mswindows/osgeo4w/lib:$PWD/mswindows/osgeo4w:/c/windows32/system32:/c/windows:/c/windows32/system32:/c/windows
+    rm -f $srcenv $dstenv $diffenv
+}
+
 function compile {
     SRC_ADDONS=$1
     SRC_GRASS=$2
     DST_DIR=$3
 
-    if [ `echo $SRC_GRASS | cut -d '/' -f6 | sed 's/grass//g'` -ge 780 ]; then
-	alias python=python3
-	py_ver=37
-    else
-	py_ver=27
+    fetchenv $OSGEO4W_ROOT_MSYS/bin/o4w_env.bat
+    export PATH=${PATH}:/c/msys${PLATFORM}/usr/bin:/c/msys${PLATFORM}/mingw${PLATFORM}/bin
+    grass_version=`echo $SRC_GRASS | cut -d '/' -f6 | sed 's/grass//g'`
+    if [ ${grass_version:1:1} -ge 8 ]; then
+	# alias python=python3
+	# py_ver=37
+	fetchenv $OSGEO4W_ROOT_MSYS/bin/py3_env.bat
+    # else
+    # 	py_ver=27
     fi
 
-    export PYTHONHOME=/c/OSGeo4W${PLATFORM}/apps/Python${py_ver}
-    export PATH=${PYTHONHOME}:${PYTHONHOME}/Scripts:${PATH}
+    # export PYTHONHOME=/c/OSGeo4W${PLATFORM}/apps/Python${py_ver}
+    # export PATH=${PYTHONHOME}:${PYTHONHOME}/Scripts:${PATH}
 
     rm -rf $DST_DIR
     $SRC_PATH/tools/addons/compile.sh $SRC_ADDONS $SRC_GRASS $DST_DIR 1
@@ -73,8 +95,9 @@ if test -z $2 ; then
     # compile ${SRC_PATH}/grass7 ${GISBASE_PATH}/grass744        ${ADDON_PATH}/grass744/${PLATFORM_DIR}/addons    
     # compile ${SRC_PATH}/grass7 ${GISBASE_PATH}/grass760        ${ADDON_PATH}/grass760/${PLATFORM_DIR}/addons
     # compile ${SRC_PATH}/grass7 ${GISBASE_PATH}/grass761        ${ADDON_PATH}/grass761/${PLATFORM_DIR}/addons    
-    compile ${SRC_PATH}/grass7 ${GISBASE_PATH}/grass780        ${ADDON_PATH}/grass780/${PLATFORM_DIR}/addons
-    compile ${SRC_PATH}/grass7 ${GISBASE_PATH}/grass781        ${ADDON_PATH}/grass781/${PLATFORM_DIR}/addons
+    compile ${SRC_PATH}/grass7 ${GISBASE_PATH}/grass780  ${ADDON_PATH}/grass780/${PLATFORM_DIR}/addons
+    compile ${SRC_PATH}/grass7 ${GISBASE_PATH}/grass781  ${ADDON_PATH}/grass781/${PLATFORM_DIR}/addons
+    compile ${SRC_PATH}/grass7 ${GISBASE_PATH}/grass782  ${ADDON_PATH}/grass782/${PLATFORM_DIR}/addons
     # compile ${SRC_PATH}/grass7 ${GISBASE_PATH}/grass74  ${ADDON_PATH}/grass74/${PLATFORM_DIR}/addons
     # compile ${SRC_PATH}/grass7 ${GISBASE_PATH}/grass76  ${ADDON_PATH}/grass76/${PLATFORM_DIR}/addons
     compile ${SRC_PATH}/grass7 ${GISBASE_PATH}/grass78  ${ADDON_PATH}/grass78/${PLATFORM_DIR}/addons
