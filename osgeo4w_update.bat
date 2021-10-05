5d79ede9a6c2a62d89a27173108ab6d550783c36
Martin Landa <landa.martin@gmail.com>
2021-10-05 17:07:55 +0200

    update osgeo4w_update script to v2

--- osgeo4w_update.bat
+++ osgeo4w_update.bat
@@ -1,24 +1,18 @@
 @echo off
 
-cd C:\Users\landa\grass_packager
+cd C:\Users\landamar\grass_packager
 
 REM
 echo Update OSGeo4W installations
 REM
 
-REM 32bit
-call:updateOSGeo4W x86 32
-call:updateOSGeo4W x86 32_grass7
-call:updateOSGeo4W x86 32_grass7_py3
-
-REM 64bit
-call:updateOSGeo4W x86_64 64
-call:updateOSGeo4W x86_64 64_grass7
-call:updateOSGeo4W x86_64 64_grass7_py3
+call:updateOSGeo4W 
+call:updateOSGeo4W _grass7
+call:updateOSGeo4W _grass8
 
 exit /b %ERRORLEVEL%
 
 :updateOSGeo4W
-	echo ...(%~1 / %~2)
-	osgeo4w-setup -g -k -a %~1 -q -R C:\OSGeo4W%~2 -s http://download.osgeo.org/osgeo4w
+	echo ...(%~1)
+	osgeo4w-setup -g -k -a x86_64 -q -R C:\OSGeo4W%~1 -s http://download.osgeo.org/osgeo4w/v2
 exit /b 0
