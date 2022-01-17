a217237b77bcd17e4cf039a291fabe0377340fed
Martin Landa <landa.martin@gmail.com>
2022-01-17 21:46:57 +0100

    copy: introduce platform

--- grass_copy_scp.sh
+++ grass_copy_scp.sh
@@ -6,17 +6,21 @@ function copy {
     SOURCE_DIR=${HOME}/grass$1
     # wingrass.fsv.cvut.cz
     HOST=landamar@wingrass
-   
+
+    PLATFORM=""
+    if [ ${1:0:1} != "8" ] ; then
+	PLATFORM="x86_64"
+    fi
     if [ ${#1} == 2 ] ; then
 	VERSION=${1:0:1}.${1:1:2}.dev      # daily
-	TARGET_DIR=/var/www/wingrass/grass$1/x86_64
-	# scp $SOURCE_DIR/WinGRASS-* $HOST:$TARGET_DIR/
+	TARGET_DIR=/var/www/wingrass/grass$1/$PLATFORM
+	scp $SOURCE_DIR/WinGRASS-* $HOST:$TARGET_DIR/
 	ssh $HOST mkdir -p $TARGET_DIR/osgeo4w/
 	scp $SOURCE_DIR/grass-*.tar.bz2 $HOST:$TARGET_DIR/osgeo4w/
 	scp -r $SOURCE_DIR/log-* $HOST:$TARGET_DIR/logs
     else 
 	VERSION=${1:0:1}.${1:1:1}.${1:2:4} # release
-	TARGET_DIR=/var/www/wingrass/grass${1:0:1}${1:1:1}/x86_64
+	TARGET_DIR=/var/www/wingrass/grass${1:0:1}${1:1:1}/$PLATFORM
     fi
     ssh $HOST mkdir -p $TARGET_DIR/addons/grass-$VERSION/
     scp -r $SOURCE_DIR/addons/*.zip $SOURCE_DIR/addons/*.md5sum $SOURCE_DIR/addons/logs/ $HOST:$TARGET_DIR/addons/grass-$VERSION/
@@ -27,15 +31,21 @@ function copy_release {
     GVERSION=$1
     HOST=martinl@grass.osgeo.org
     SOURCE_DIR=${HOME}/grass$GVERSION
+    VERSION=${GVERSION:0:1}${GVERSION:1:1}
+    PLATFORM=""
+    if [ ${1:0:1} != "8" ] ; then
+	PLATFORM="x86_64"
+    fi
+    
     scp $SOURCE_DIR/WinGRASS-* $HOST:
-    # scp $SOURCE_DIR/grass-*.tar.bz2 $HOST:/osgeo/download/osgeo4w/v2/x86_64/release/grass/
-    # ssh $HOST ./copy_binaries.sh
+    # scp $SOURCE_DIR/grass-*.tar.bz2 $HOST:/osgeo/download/osgeo4w/v2/$PLATFORM/release/grass/
+    ssh $HOST scp WinGRASS-* grass.lxd:wingrass$VERSION
+    ssh $HOST rm WinGRASS-*
     
     # addons
     HOST=landamar@wingrass
-    VERSION=${GVERSION:0:1}${GVERSION:1:1}
     FVERSION=${GVERSION:0:1}.${GVERSION:1:1}.${GVERSION:2:2}
-    TARGET_DIR=/var/www/wingrass/grass$VERSION/x86_64
+    TARGET_DIR=/var/www/wingrass/grass$VERSION/$PLATFORM
     # ssh $HOST mkdir $TARGET_DIR/addons/grass-$FVERSION
     # scp -r $SOURCE_DIR/addons/* $HOST:$TARGET_DIR/addons/grass-$FVERSION/
     # TBD: only for final release
@@ -49,7 +59,7 @@ if test -z $1 ; then
     copy 78
     copy 786
     copy 80
-    copy 800RC1
+    copy 800RC2
     copy 81    
 else
     copy_release $1
