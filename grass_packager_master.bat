52f2eae5b9addad1a675074a1298107293d67b60
Martin Landa <landa.martin@gmail.com>
2022-01-17 21:08:05 +0100

    packager master partially updated

--- grass_packager_master.bat
+++ grass_packager_master.bat
@@ -1,53 +1,48 @@
 @echo off
 
-cd C:\Users\landa\grass_packager
+set HOME=C:\Users\landamar\grass_packager
+cd %HOME%
 
 REM
 echo Clean-up...
 REM
-REM call :cleanUp 32
-call :cleanUp 64
+REM call :cleanUp
 
 REM
 echo Compiling GRASS GIS...
 REM
-REM C:\msys32\usr\bin\bash.exe .\grass_compile.sh 32 76 -daily
-C:\msys64\usr\bin\bash.exe .\grass_compile.sh 64 80
+C:\msys64\usr\bin\bash.exe .\grass_compile.sh 81
 
 pause
 
 REM
 echo Clean-up for packaging...
 REM
-REM call:cleanUpPkg x86    32
-REM call:cleanUpPkg x86_64 64
+call:cleanUpPkg 
 
 REM
 echo Preparing packages...
 REM
-REM call:preparePkg x86    32
-REM call:preparePkg x86_64 64
+call:preparePkg
 
 REM
 echo Finding latest package and update info...
 REM
-REM C:\msys32\usr\bin\bash.exe .\grass_osgeo4w.sh  32
-C:\msys64\usr\bin\bash.exe .\grass_osgeo4w.sh  64 80
-REM C:\msys32\usr\bin\bash.exe .\grass_rev_info.sh 32 73
-C:\msys64\usr\bin\bash.exe .\grass_rev_info.sh 64 80
+C:\msys64\usr\bin\bash.exe .\grass_osgeo4w.sh  81
+C:\msys64\usr\bin\bash.exe .\grass_rev_info.sh 81
 
-pause 
 REM
 echo Creating standalone installer...
 REM
-REM call:createPkg x86
-REM call:createPkg x86_64
+call:createPkg
+
+pause
 
 REM
 REM Create md5sum files
 REM
 REM C:\msys32\usr\bin\bash.exe .\grass_md5sum.sh 32 73
-C:\msys64\usr\bin\bash.exe .\grass_md5sum.sh 64 80
+C:\msys64\usr\bin\bash.exe .\grass_md5sum.sh 81
 
 REM
 echo Building addons...
@@ -66,25 +61,20 @@ REM C:\msys64\usr\bin\bash.exe .\grass_copy_wwwroot.sh 64
 exit /b %ERRORLEVEL%
 
 :cleanUp
-	echo ...(%~1)
-        for /d %%G in ("C:\OSGeo4W%~1\apps\grass\grass-7*svn") do rmdir /S/Q "%%G"
+        REM for /d %%G in ("C:\OSGeo4W%~1\apps\grass\grass7*") do rmdir /S/Q "%%G"
 exit /b 0
 
 :cleanUpPkg
-	echo ...(%~1)
-	if not exist "grass80" mkdir grass80
-	if exist .\grass80\%~1 rmdir /S/Q .\grass80\%~1
-	xcopy C:\msys%~2\usr\src\grass80\mswindows\*     .\grass80\%~1 /S/V/I > NUL
+	if exist .\grass81 rmdir /S/Q .\grass81
+	xcopy C:\msys64\usr\src\grass81\mswindows\*     .\grass81 /S/V/I > NUL
 exit /b 0
 
 :preparePkg
-	echo ...(%~1)
-	cd .\grass80\%~1
-	call .\GRASS-Packager.bat %~2 > .\GRASS-Packager.log
-	cd ..\..
+	cd .\grass81
+	call .\GRASS-Packager.bat > .\GRASS-Packager.log
+	cd ..
 exit /b 0
 
 :createPkg
-	echo ...(%~1)
-        C:\DevTools\makensis.exe .\grass80\%~1\GRASS-Installer.nsi > .\grass80\%~1\GRASS-Installer.log
+        C:\DevTools\makensis.exe .\grass81\GRASS-Installer.nsi > .\grass81\GRASS-Installer.log
 exit /b 0
